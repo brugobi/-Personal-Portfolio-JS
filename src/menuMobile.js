b54e4fbd4b8778a9914b0a302b52d6a7d7b69919
brugobi <brgobbi@hotmail.com>
2021-02-10 12:33:15 -0500

    delete menuMobile.js file

--- src/menuMobile.js
+++
@@ -1,6 +0,0 @@
-const bugerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-bugerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
-});
