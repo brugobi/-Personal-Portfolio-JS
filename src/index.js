2b7600ba745c453a49f43f6e729a7f46ba7e5e86
brugobi <brgobbi@hotmail.com>
2021-02-18 14:33:47 -0500

    add js function to the projects section by add action to the next-btn and prev-btn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import './styles.scss';
+import projects from './projects';
 import Trip from './assets/images/trip.jpg';
+import News from './assets/images/news_week.png';
+import Shop from './assets/images/online_shop.png';
 import Background from './assets/images/project.png';
 
 const bugerIcon = document.querySelector('#burger');
@@ -10,6 +13,45 @@ bugerIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active');
 });
 
- 
+// projects
+const title = document.getElementById('title-project');
+const img = document.getElementById('project-img');
+const info = document.getElementById('info');
+const linkSource = document.getElementById('link-source');
+const linkDemo = document.getElementById('link-demo');
 
+let currentItem = 0;
 
+const showPerson = () => {
+  const item = projects[currentItem];
+  img.src = item.img;
+  title.textContent = item.title;
+  info.textContent = item.text;
+  linkDemo.href = item.live;
+  linkSource.href = item.source;
+};
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson();
+});
+
+// show next person
+nextBtn.addEventListener('click', () => {
+  currentItem += 1;
+  if (currentItem > projects.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// show prev person
+prevBtn.addEventListener('click', () => {
+  currentItem -= 1;
+  if (currentItem < 0) {
+    currentItem = projects.length - 1;
+  }
+  showPerson(projects);
+});
--- src/projects.js
+++ src/projects.js
@@ -3,33 +3,61 @@ const projects = [
     id: 1,
     title: 'Trip Tips Canada',
     tool: ['Ruby on Rails', 'HTML/CSS'],
-    img: './assets/images/trip.png',
+    img: './trip.jpg',
+    source: 'https://github.com/brugobi/RoR--Capstone',
+    live: 'https://trip-tips-canada.herokuapp.com/',
     text:
       'This is a Desktop version of a blog where the user can create articles to share informations about traveling in Canada.This is a Desktop version of a blog where the user can create articles to share informations about traveling in Canada.',
   },
   {
     id: 2,
-    title: 'The Newsweek Clone',
-    tool: ['Bootstrap', 'HTML/CSS'],
-    img: './assets/images/trip.png',
+    title: 'The Walkers',
+    tool: ['JavaScript', 'Phaser3', 'HTML/CSS'],
+    img: './trip.jpg',
+    source: 'https://github.com/brugobi/phaser-capstone',
+    live: 'https://brugobi.github.io/phaser-capstone/',
     text:
-      'This project requests to build a responsive replica of the news site Newsweek  using the Bootstrap framework.',
+      'This is a desktop shoot game implemented it using Phaser. For developing the game. I implemented an API provided by Microverse Program to host the scoring database so that the highest scores will always be available for new users to try to beat!',
   },
   {
     id: 3,
-    title: 'Tic Tac Toe',
-    tool: ['Ruby/OOP', 'Rubocop'],
-    img: './assets/images/trip.png',
+    title: 'Weather App',
+    tool: ['JavaScrip', 'APIs', 'Bulma', 'HTML/CSS'],
+    img: './trip.jpg',
+    source: 'https://github.com/brugobi/Weather-App-JS',
+    live: 'https://raw.githack.com/brugobi/Weather-App-JS/feature_v1/dist/index.html',
     text:
-      'In this project we tried to build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.',
+      'This is a responsive weather app page built with JavaScript and APIs: Get weather information by getting the user location if the user accepts it, Get weather information by typing the city name, Autocomplete functionality for cities names and, Temperature available in Celsius and Fahrenheit by clicking on the temperature number.',
   },
   {
     id: 4,
+    title: 'The Newsweek Clone',
+    tool: ['Bootstrap', 'HTML/CSS'],
+    img: './news_week.png',
+    source: 'https://github.com/brugobi/Newsweek-clone',
+    live: 'https://raw.githack.com/brugobi/Newsweek-clone/master/index.html',
+    text:
+      'This project requests students to build a replica of the news site Newsweek (https://www.newsweek.com/) using the Bootstrap framework..',
+  },
+  {
+    id: 5,
+    title: 'Restaurant Page',
+    tool: ['JavaScrip', 'Node.js', 'Webpack', 'Boostrap', 'HTML/CSS'],
+    img: './trip.jpg',
+    source: 'https://github.com/brugobi/restaurant_page-js',
+    live: 'https://raw.githack.com/brugobi/restaurant_page-js/feature/dist/index.html',
+    text:
+      'This is a responsive Restaurant page built with entire JavaScript.',
+  },
+  {
+    id: 6,
     title: 'Online Shop',
     tool: ['HTML&CSS', 'Bootstrap'],
-    img: './assets/images/trip.png',
+    img: './online_shop.png',
+    source: 'https://github.com/brugobi/online-shop',
+    live: 'https://raw.githack.com/brugobi/online-shop/master/index.html',
     text:
-      'This project is about building a responsive webpage based on an online shop for electronics using HTML and CSS. Design idea by Mohammed Awad on Behance . Based on the mentioned template I decided to customize it and build a board game store. There are two pages: the main one and the search result one.',
+      'This project is about building a webpage based on an online shop for electronics using HTML and CSS. Design idea by Mohammed Awad on Behance and you can check out on this link (https://www.behance.net/gallery/24796463/ZATTIX). Based on the mentioned template I decided to customize it and build a board game store. There are two pages: the main one and the search result one.',
   },
 ];
 
